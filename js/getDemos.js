8a92f162d08f4da7fc8639507ac4c4c8b0805d08
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-04-08 00:23:04 -0400

    recursive project.xml finding

--- js/getDemos.js
+++ js/getDemos.js
@@ -3,10 +3,25 @@ const path = require('path');
 
 module.exports = ({core}) => {
     try {
-        const demos = fs.readdirSync('.').filter(file => fs.statSync(file).isDirectory());
-        const projectPaths = demos.map(demo => path.join(demo, 'project.xml'));
-        core.setOutput('project-paths', projectPaths.join('\n'));
-        console.log('project-paths:', projectPaths.join('\n'));
+        let projectPaths = [];
+        function findProjectXmlFiles(dir) {
+            const filesAndDirs = fs.readdirSync(dir, {withFileTypes: true});
+
+            for (const item of filesAndDirs) {
+                const fullPath = path.join(dir, item.name);
+                if (item.isDirectory())
+                    findProjectXmlFiles(fullPath);
+                else if (item.isFile() && item.name === 'project.xml')
+                    projectPaths.push(fullPath);
+            }
+        }
+
+        const rootDir = "./demos/";
+
+        findProjectXmlFiles(rootDir);
+
+        core.setOutput('project-paths', projectPaths);
+        console.log('project-paths:', projectPaths);
       } catch (error) {
         core.setFailed(error.message);
       }
